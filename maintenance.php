907d7061a209ee350b04d6c78158daf3e5e527c6
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-05 15:21:22 +0100

    Update maintenance.php

--- maintenance.php
+++ maintenance.php
@@ -14,7 +14,7 @@ if (isAdmin()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maintenance - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
     <style>
         @keyframes spin {
             0% { transform: rotate(0deg); }
@@ -90,4 +90,5 @@ if (isAdmin()) {
         </div>
     </div>
 </body>
+
 </html>
