bca93d6b7f7765174946f22d165bd4cbbe973844
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-10 09:47:42 +0100

    Update grades.php

--- grades.php
+++ grades.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Grades - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include 'includes/header.php'; ?>
@@ -87,3 +87,4 @@
     <?php include 'includes/footer.php'; ?>
 </body>
 </html>
+
