d10248fe54bc01f3f8baae461437b67e8c6d5fe3
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-14 13:42:19 +0100

    Update login.php

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion Admin - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include 'includes/header.php'; ?>
@@ -87,4 +87,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php include 'includes/footer.php'; ?>
 </body>
 </html>
+
 
