dab05d42193ffd9afa9415761db2d27fb404662c
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-14 13:38:16 +0100

    Update recruitment.php

--- recruitment.php
+++ recruitment.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recrutement - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include 'includes/header.php'; ?>
@@ -40,4 +40,5 @@
 
     <?php include 'includes/footer.php'; ?>
 </body>
+
 </html>
