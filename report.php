154507b5922ad1cee698bd8a0dd81bca55b0c352
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-14 13:41:49 +0100

    Update report.php

--- report.php
+++ report.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signaler un Problème - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include 'includes/header.php'; ?>
@@ -84,4 +84,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <?php include 'includes/footer.php'; ?>
 </body>
+
 </html>
