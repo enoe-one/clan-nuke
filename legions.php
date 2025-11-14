1132307d232d9fc84a7590d62fb82045cd24432d
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-14 13:43:04 +0100

    Update legions.php

--- legions.php
+++ legions.php
@@ -18,7 +18,7 @@ $legions = $stmt->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Légions - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include 'includes/header.php'; ?>
@@ -157,4 +157,5 @@ $legions = $stmt->fetchAll();
     <?php include 'includes/footer.php'; ?>
 </body>
 </html>
+
 
