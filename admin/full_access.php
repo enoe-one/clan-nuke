f3eb6ae1f0baf7ab76136a05383cae42a0aabaf0
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-25 16:57:57 +0100

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- admin/full_access.php
+++ admin/full_access.php
@@ -221,7 +221,7 @@ $stats = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accès Complet - CFWT Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+ <link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include '../includes/header.php'; ?>
@@ -869,3 +869,4 @@ $stats = [
     <?php include '../includes/footer.php'; ?>
 </body>
 </html>
+
