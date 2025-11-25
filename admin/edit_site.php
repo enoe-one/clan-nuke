39fc6b7f13cefc26ebbe203fb42ce51244cb5924
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-25 15:12:40 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- admin/edit_site.php
+++ admin/edit_site.php
@@ -283,7 +283,7 @@ $stats = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Édition Site - CFWT Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include '../includes/header.php'; ?>
@@ -1141,3 +1141,4 @@ $stats = [
     <?php include '../includes/footer.php'; ?>
 </body>
 </html>
+
