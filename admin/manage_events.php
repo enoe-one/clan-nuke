69ad68a9cde3893930cc21a819607f1ad6b8d58d
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-26 09:49:41 +0100

    Update Font Awesome stylesheet link

--- admin/manage_events.php
+++ admin/manage_events.php
@@ -127,7 +127,7 @@ $grades = getGrades();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Événements - CFWT Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
 <?php include '../includes/header.php'; ?>
