302fee6c8e90805a726a8c90e5f891ba15cc2977
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-11-26 09:52:08 +0100

    Update dashboard.php

--- member/dashboard.php
+++ member/dashboard.php
@@ -54,7 +54,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Profil - CFWT</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link rel="stylesheet" href="css/all.min.css">
 </head>
 <body class="bg-gray-900">
     <?php include '../includes/header.php'; ?>
@@ -219,6 +219,7 @@ try {
     <?php include '../includes/footer.php'; ?>
 </body>
 </html>
+
 
 
 
